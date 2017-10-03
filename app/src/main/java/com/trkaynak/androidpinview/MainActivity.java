36190c7b70084b1d0ec9c1a950f3b407c388d2cf
fatih acar <fatihacar79@gmail.com>
2017-10-03 15:51:15 +0300

    Initial commit

---
+++ app/src/main/java/com/trkaynak/androidpinview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.trkaynak.androidpinview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.goodiebag.pinview.Pinview;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Pinview pinview=(Pinview) findViewById(R.id.pinview);
+
+        pinview.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean b) {
+                Toast.makeText(MainActivity.this,""+pinview.getValue(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
